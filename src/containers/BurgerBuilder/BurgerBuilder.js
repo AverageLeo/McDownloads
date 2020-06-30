401aa97b5a2e12f33cf7f7f9a2ef130f75626c03
AverageLeo <Arieka39@gmail.com>
2020-06-30 16:08:32 +0300

    added burger component with css

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import Auxillery from "../../hoc/Auxillery";
+import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
   render() {
     return (
       <Auxillery>
-        <div>Burger</div>
+        <Burger />
         <div>Build Controls</div>
       </Auxillery>
     );
