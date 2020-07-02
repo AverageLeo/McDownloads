65a999950229a756bd7553b2e3e712b6cb01de11
AverageLeo <Arieka39@gmail.com>
2020-07-02 15:47:06 +0300

    added backdrop to ordersummary

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,16 +1,21 @@
 import React from "react";
 import classes from "./Modal.css";
+import Auxillery from "../../../hoc/Auxillery";
+import Backdrop from "../Backdrop/Backdrop";
 
 const modal = (props) => (
-  <div
-    className={classes.Modal}
-    style={{
-      opacity: props.show ? "1" : "0",
-      transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-    }}
-  >
-    {props.children}
-  </div>
+  <Auxillery>
+    <Backdrop show={props.show} clicked={props.modalClosed} />
+    <div
+      className={classes.Modal}
+      style={{
+        opacity: props.show ? "1" : "0",
+        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
+      }}
+    >
+      {props.children}
+    </div>
+  </Auxillery>
 );
 
 export default modal;
